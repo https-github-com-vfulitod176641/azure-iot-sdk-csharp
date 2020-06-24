685a77c4afb9c89cb10faebaf78b25fc5011dea6
Abhipsa Misra <abhipsa.misra@microsoft.com>
2020-06-24 16:02:28 -0700

    initi proj

---
+++ iothub/device/samples/PnpDeviceSamples/PnpDeviceSamples/Thermostat/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Thermostat
+{
+    class Program
+    {
+        private const string DeviceConnectionString = "device_connection_string_here";
+        private const string ModelId = "dtmi:com:example:simplethermostat;1";
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public async Task RunSampleAsync()
+        {
+
+        }
+    }
+}
